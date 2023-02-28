f4d6766b7ab879a7e8b81a095b84d9fa8af2751f
Basli, Adel <Adel.Basli@nielseniq.com>
2023-02-28 14:53:24 -0600

    add filters

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ print(df.groupby('CASE_STATUS')['DECISION_TIME'].mean())
 
 # Get the mean decision time for all cases
 print(df['DECISION_TIME'].mean())
-
+#%%
 # Keep only the first 7 character of SOC codes
 df['PW_SOC_CODE'] = df['PW_SOC_CODE'].str[:7]
 
@@ -31,6 +31,7 @@ soc_codes = ['15-2031']
 df_soc = df[df['PW_SOC_CODE'].isin(soc_codes)]
 print(df_soc.groupby('CASE_STATUS')['DECISION_TIME'].mean())
 
+#%%
 # Get the mean decision time for a given law firm
 law_firms = 'fragomen'
 
@@ -40,3 +41,9 @@ df_law['AGENT_ATTORNEY_FIRM_NAME'] = df_law['AGENT_ATTORNEY_FIRM_NAME'].str.lowe
 df_law = df_law[df_law['AGENT_ATTORNEY_FIRM_NAME'].str.contains(law_firms)]
 print(df_law['CASE_STATUS'].value_counts(normalize=True))
 print(df_law['DECISION_TIME'].mean())
+
+#%%
+# Get the approval rate by waiting time
+days_pending = 280
+df_pending = df[(df['DECISION_TIME'] >= days_pending - 5) & (df['DECISION_TIME'] <= days_pending + 5)]
+print(df_pending['CASE_STATUS'].value_counts(normalize=True))
